801dddbf63e847336c80c1f22e572b69a3c63ad6
Bogdan <twist093@gmail.com>
2014-04-11 10:28:35 +0200

    SimpleList: Simple list implemented

--- SpringApp/src/main/java/pw/bshkola/mvc/controller/MoviesListController.java
+++ SpringApp/src/main/java/pw/bshkola/mvc/controller/MoviesListController.java
@@ -1,30 +1,20 @@
 package pw.bshkola.mvc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/welcome")
-public class MoviesListController {
-
-	@RequestMapping(method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
-
-		model.addAttribute("message", "Spring 3 MVC Hello World");
-		return "moviesList";
-
-	}
-	
-}
-
-/*@Controller
 @RequestMapping(value="/movies")
 public class MoviesListController {
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView getMoviesList(ModelAndView model) {
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+
 		List<String> moviesList = new ArrayList<String>();
 
 		moviesList.add("Fast and Furious");
@@ -33,10 +23,10 @@ public class MoviesListController {
 		moviesList.add("Lord of the Rings: Return of the King");
 		moviesList.add("The Dark Knight");
 		
-		model.addObject("moviesList", moviesList);
-		model.setViewName("moviesList");
-		return model;
+		model.addAttribute("moviesList", moviesList);
+
+		return "moviesList";
+
 	}
 	
 }
-*/
--- SpringApp/src/main/webapp/WEB-INF/views/jsp/moviesList.jsp
+++ SpringApp/src/main/webapp/WEB-INF/views/jsp/moviesList.jsp
@@ -1,5 +1,16 @@
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <html>
 <body>
-	<h1>Message : ${message}</h1>	
+	<h2>Spring MVC and List Example</h2>
+ 
+	<c:if test="${not empty moviesList}">
+ 
+		<ul>
+			<c:forEach var="listValue" items="${moviesList}">
+				<li>${listValue}</li>
+			</c:forEach>
+		</ul>
+ 
+	</c:if>
 </body>
 </html>
